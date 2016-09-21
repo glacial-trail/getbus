14f2919ac8b854d4eec8b5fa7fbb158d999336e7
rain <crystalline.passage@gmail.com>
2016-09-21 04:19:29 +0300

    now with freemarker

--- servebus/src/main/java/info/getbus/servebus/web/controllers/MainController.java
+++ servebus/src/main/java/info/getbus/servebus/web/controllers/MainController.java
@@ -1,7 +1,10 @@
 package info.getbus.servebus.web.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
@@ -13,4 +16,16 @@ public class MainController {
         return "bus";
     }
 
+    @RequestMapping(value = "/fget/", method = RequestMethod.GET)
+    public String fget(ModelMap model) {
+        model.addAttribute("title", "get");
+        model.addAttribute("free", "bus");
+        return "fget";
+    }
+    @RequestMapping(value = "/mfget/", method = RequestMethod.GET)
+    public String mfget(@ModelAttribute("model") ModelMap model) {
+        model.addAttribute("title", "mget");
+        model.addAttribute("free", "mbus");
+        return "fget";
+    }
 }
---
+++ servebus/src/main/webapp/WEB-INF/views/ftl/fget.ftl
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head lang="en">
+    <meta charset="UTF-8">
+    <title>${model.title}</title>
+</head>
+<body>
+    <h2>${model.free}</h2>
+</body>
+</html>
